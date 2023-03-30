2ccff6c6b61b4b7992f933f7b17f27c548249d76
zmahmood98 <zahra.mahmood98@hotmail.co.uk>
2023-03-30 12:16:57 +0100

    update

---
+++ src/components/Input/index.js
@@ -0,0 +1,30 @@
+import React, { useEffect } from 'react';
+import './index.css';
+
+
+const Input = () => {
+
+    return (
+        <>
+        <div class="input-div">
+            <fieldset class="govuk-fieldset">
+                <legend class="govuk-fieldset__legend govuk-fieldset__legend--l">
+                    <h1 class="govuk-fieldset__heading">
+                    What is your URL?
+                    </h1>
+                </legend>
+                <div class="govuk-form-group">
+                    <label class="govuk-label" for="address-line-1">
+                    URL
+                    </label>
+                    <input class="govuk-input" id="address-line-1" name="address-line-1" type="text" autocomplete="address-line1" />
+                </div>
+                </fieldset>
+        </div>
+        </>
+    )
+
+}
+
+export default Input;
+
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import './index.css';
+import { Input } from '../../components'
 
 function Home () {
     return (
         <>
                 <div className='home-div'>
                     <h1 class="govuk-heading-l">URL Scan</h1>
-                    
-                    <p class="govuk-body-l">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
                     <p class="govuk-body-l">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                 </div>
+
+                <Input />
         </>
     );
 }
