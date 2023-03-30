7962d9a62a21343040fc881fd3cdd1d384abd63d
zmahmood98 <zahra.mahmood98@hotmail.co.uk>
2023-03-30 17:37:53 +0100

    update

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -6,24 +6,7 @@ const Input = () => {
 
     return (
         <>
-        <div class="input-div">
-            <fieldset class="govuk-fieldset">
-                <legend class="govuk-fieldset__legend govuk-fieldset__legend--l">
-                    <h1 class="govuk-fieldset__heading">
-                    What is your URL?
-                    </h1>
-                </legend>
-                <div class="govuk-form-group">
-                    <label class="govuk-label" for="address-line-1">
-                    URL
-                    </label>
-                    <input class="govuk-input" id="address-line-1" name="address-line-1" type="text" autocomplete="address-line1" />
-                </div>
-                </fieldset>
-                <button class="govuk-button" data-module="govuk-button">
-                    Scan
-                </button>
-        </div>
+        
 
         </>
     )
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,35 @@
 import React, { useState, useEffect, useRef, Component } from 'react';
 import axios from "axios";
 import './index.css';
-import { Input } from '../../components'
 
 function Home () {
 
-    const api = axios.create({
-        baseURL: "http://localhost:8080/score"
-    });
+    const handleSubmit = e => {
+        e.preventDefault()
+    
+        // Handle validations
+        axios.post({localhost:8080/score}, {url})
+        .then(response => {
+            console.log(response)
+            // take score from the response - setScore to that
+            setScore(response.score)
+        })
+
+        if (score<=30) {
+            console.log('low chance');
+            setDetails('It is not very likely that this link is part of a phishing scam.')
+        } else if (score >= 60) {
+            console.log('high chance');
+            setDetails('It is extremely likely that this link is part of a phishing scam.')
+        } else {
+            console.log('medium chance');
+            setDetails('We currently cannot evaluate accurately whether or not this link is part of a phishing scam.')
+        }
+    }
+
+    const [url, setUrl] = useState()
+    const [score, setScore] = useState()
+    const [details, setDetails] = useState()
 
     return (
         <>
@@ -18,7 +40,25 @@ function Home () {
                         <p className="govuk-body-l">The following service will scan any URL inputted and produce a percentage score to inform you of how likely it is that the link is part of a phishing scam. </p>
                     </div>
 
-                    <Input />
+                    <form class="input-div" method="post" onSubmit={handleSubmit}>
+                        <fieldset class="govuk-fieldset">
+                            <legend class="govuk-fieldset__legend govuk-fieldset__legend--l">
+                                <h1 class="govuk-fieldset__heading">
+                                What is your URL?
+                                </h1>
+                            </legend>
+                            <div class="govuk-form-group">
+                                <label class="govuk-label" for="address-line-1">
+                                URL
+                                </label>
+                                <input class="govuk-input" id="address-line-1" name="address-line-1" type="text" autocomplete="address-line1" value={url} onChange={e => setUrl(e.target.value)}/>
+                            </div>
+                        </fieldset>
+                        <button class="govuk-button" data-module="govuk-button" type="submit">
+                            Scan
+                        </button>
+                    </form>
+                    
                 </section>
 
                 <section className='govuk-width-container'>
@@ -29,7 +69,7 @@ function Home () {
                             URL
                             </dt>
                             <dd class="govuk-summary-list__value">
-                            [dynamic content]
+                            {url}
                             </dd>
                         </div>
                         <div class="govuk-summary-list__row">
@@ -37,7 +77,7 @@ function Home () {
                             Percentage Score
                             </dt>
                             <dd class="govuk-summary-list__value">
-                            [dc]%
+                            {score}%
                             </dd>
                         </div>
                         <div class="govuk-summary-list__row">
@@ -45,15 +85,13 @@ function Home () {
                             Details
                             </dt>
                             <dd class="govuk-summary-list__value">
-                            <p class="govuk-body">It is [dc] that this link is part of a phishing scam.</p>
+                            <p class="govuk-body">{details}</p>
                             </dd>
                         </div>
                         </dl>
                 </section>
-
-
         </>
     );
 }
-  
-  export default Home;
+
+export default Home;
