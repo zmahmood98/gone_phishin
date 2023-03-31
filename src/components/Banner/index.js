f93ce8a700d6f0eb680cdc9d47859870eb2d6089
zmahmood98 <zahra.mahmood98@hotmail.com>
2023-03-31 14:02:02 +0100

    update banner

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -6,32 +6,37 @@ const Banner = () => {
 
     return (
       <>
-      <section className="govuk-width-container container">
-
-        <div
-          class="govuk-notification-banner"
-          role="region"
-          aria-labelledby="govuk-notification-banner-title"
-          data-module="govuk-notification-banner"
-        >
-          <div class="govuk-notification-banner__header">
-            <h2
-              class="govuk-notification-banner__title"
-              id="govuk-notification-banner-title"
-            >
-              Important
-            </h2>
+        <section className="govuk-width-container container">
+          <div
+            class="govuk-notification-banner"
+            role="region"
+            aria-labelledby="govuk-notification-banner-title"
+            data-module="govuk-notification-banner"
+          >
+            <div class="govuk-notification-banner__header">
+              <h2
+                class="govuk-notification-banner__title"
+                id="govuk-notification-banner-title"
+              >
+                Important
+              </h2>
+            </div>
+            <div class="govuk-notification-banner__content">
+              <p class="govuk-notification-banner__heading">
+                This website's server is currently not deployed, so results will
+                not display correctly at this time. If you would like to run the
+                server locally please click &nbsp;
+                <a
+                  class="govuk-notification-banner__link"
+                  href="https://github.com/zmahmood98/gone-phishin-xgov-hackathon"
+                >
+                  here
+                </a>
+                .
+              </p>
+            </div>
           </div>
-          <div class="govuk-notification-banner__content">
-            <p class="govuk-notification-banner__heading">
-              This website's server is currently still in production.
-              {/* <a class="govuk-notification-banner__link" href="#">
-                View application
-              </a> */}
-            </p>
-          </div>
-        </div>
-      </section>
+        </section>
       </>
     );
 
